4dd8c1a2d4a72696640ed354d5508bfd567f29d3
Zi Yan <ziy@nvidia.com>
2018-01-16 18:18:59 -0500

    mm: thp: split PUD THPs at page reclaim.
    We cannot swap PUD THPs, so split them before swap them out. PUD THPs
    will be split into PMD THPs, so that if THP_SWAP is enabled, PMD THPs
    can be swapped out as a whole.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1461,8 +1461,8 @@ static unsigned int shrink_page_list(struct list_head *page_list,
 					 * tail pages can be freed without IO.
 					 */
 					if (!compound_mapcount(page) &&
-					    split_huge_page_to_list(page,
-								    page_list))
+						split_huge_page_to_list(page,
+									page_list))
 						goto activate_locked;
 				}
 try_to_swap:
@@ -1470,6 +1470,8 @@ try_to_swap:
 					if (!PageTransHuge(page))
 						goto activate_locked_split;
 					/* Fallback to swap normal pages */
+					VM_BUG_ON_PAGE(compound_order(page) != HPAGE_PMD_ORDER,
+						       page);
 					if (split_huge_page_to_list(page,
 								    page_list))
 						goto activate_locked;
@@ -1486,6 +1488,7 @@ try_to_swap:
 				mapping = page_mapping(page);
 			}
 		} else if (unlikely(PageTransHuge(page))) {
+			VM_BUG_ON_PAGE(compound_order(page) != HPAGE_PMD_ORDER, page);
 			/* Split file THP */
 			if (split_huge_page_to_list(page, page_list))
 				goto keep_locked;
@@ -1511,9 +1514,12 @@ try_to_swap:
 			enum ttu_flags flags = TTU_BATCH_FLUSH;
 			bool was_swapbacked = PageSwapBacked(page);
 
-			if (unlikely(PageTransHuge(page)))
-				flags |= TTU_SPLIT_HUGE_PMD;
-
+			if (unlikely(PageTransHuge(page))) {
+				if (compound_order(page) == HPAGE_PMD_ORDER)
+					flags |= TTU_SPLIT_HUGE_PMD;
+				else if (compound_order(page) == HPAGE_PUD_ORDER)
+					flags |= TTU_SPLIT_HUGE_PUD;
+			}
 			if (!try_to_unmap(page, flags)) {
 				stat->nr_unmap_fail += nr_pages;
 				if (!was_swapbacked && PageSwapBacked(page))
