01cce69ebd2b9207d4701b15f1b6381c0d21e47e
Zi Yan <ziy@nvidia.com>
2018-05-29 21:55:01 -0400

    sysctl: toggle to promote PUD-mapped 1GB THP or not.
    Only promotion PMD THP by default.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -125,6 +125,7 @@ extern int vma_scan_threshold_type;
 extern int vma_no_repeat_defrag;
 extern int num_breakout_chunks;
 extern int defrag_size_threshold;
+extern int mem_defrag_promote_thp;
 
 extern int only_print_head_pfn;
 
@@ -137,6 +138,7 @@ static int __maybe_unused neg_one = -1;
 static int __maybe_unused two = 2;
 static int __maybe_unused four = 4;
 static unsigned long zero_ul;
+static int __maybe_unused fifteen = 15;
 static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
@@ -1789,6 +1791,15 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "mem_defrag_promote_thp",
+		.data		= &mem_defrag_promote_thp,
+		.maxlen		= sizeof(mem_defrag_promote_thp),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= &fifteen,
 	},
 	{ }
 };
