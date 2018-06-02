214bee8e22aca806bfd6a5bfc0e25407068779ee
Zi Yan <ziy@nvidia.com>
2018-06-02 18:02:06 -0400

    mm: page_vma_walk: teach it about PMD-mapped PUD THP.
    We now have PMD-mapped PUD THP and PTE-mapped PUD THP, page_vma_walk
    should handle them properly.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/page_vma_mapped.c
+++ mm/page_vma_mapped.c
@@ -7,6 +7,12 @@
 
 #include "internal.h"
 
+enum check_pmd_result {
+	PVM_NOT_MAPPED = 0,
+	PVM_LEAF_ENTRY,
+	PVM_NONLEAF_ENTRY,
+};
+
 static inline bool not_found(struct page_vma_mapped_walk *pvmw)
 {
 	page_vma_mapped_walk_done(pvmw);
@@ -50,6 +56,25 @@ static bool map_pte(struct page_vma_mapped_walk *pvmw)
 	}
 	pvmw->ptl = pte_lockptr(pvmw->vma->vm_mm, pvmw->pmd);
 	spin_lock(pvmw->ptl);
+	return true;
+}
+
+static bool map_pmd(struct page_vma_mapped_walk *pvmw)
+{
+	pvmw->pmd = pmd_offset(pvmw->pud, pvmw->address);
+
+	if (!(pvmw->flags & PVMW_SYNC)) {
+		if (pvmw->flags & PVMW_MIGRATION) {
+			if (!thp_migration_supported() ||
+				!is_pmd_migration_entry(*pvmw->pmd))
+				return false;
+		} else {
+			if (!pmd_present(*pvmw->pmd))
+				return false;
+		}
+	}
+
+	pvmw->ptl = pmd_lock(pvmw->vma->vm_mm, pvmw->pmd);
 	return true;
 }
 
@@ -127,6 +152,56 @@ static void step_forward(struct page_vma_mapped_walk *pvmw, unsigned long size)
 }
 
 /**
+ * check_pmd - check if @pvmw->page is mapped at the @pvmw->pmd
+ *
+ * page_vma_mapped_walk() found a place where @pvmw->page is *potentially*
+ * mapped. check_pmd() has to validate this.
+ *
+ * @pvmw->pmd may point to empty PMD, migraiton PMD, PMD pointing to arbitrary
+ * huge page, or PMD pointing to a PTE page table page.
+ *
+ * If PVMW_MIGRATION flag is set, returns PVM_LEAF_ENTRY if @pvmw->pmd contains
+ * migration entry that points to @pvmw->page.
+ *
+ * If PVMW_MIGRATION flag is not set, returns PVM_LEAF_ENTRY if @pvmw->pmd
+ * points to @pvmw->page.
+ *
+ * If @pvmw->pmd points to a PTE page table page, returns PVM_NONLEAF_ENTRY.
+ *
+ * Otherwise, return PVM_NOT_MAPPED.
+ *
+ */
+static enum check_pmd_result check_pmd(struct page_vma_mapped_walk *pvmw)
+{
+	unsigned long pfn;
+
+	if (likely(pmd_trans_huge(*pvmw->pmd))) {
+		if (pvmw->flags & PVMW_MIGRATION)
+			return 0;
+		pfn = pmd_pfn(*pvmw->pmd);
+		if (!pfn_is_match(pvmw->page, pfn))
+			return PVM_NOT_MAPPED;
+		return PVM_LEAF_ENTRY;
+	} else if (!pmd_present(*pvmw->pmd)) {
+		if (thp_migration_supported()) {
+			if (!(pvmw->flags & PVMW_MIGRATION))
+				return 0;
+			if (is_migration_entry(pmd_to_swp_entry(*pvmw->pmd))) {
+				swp_entry_t entry = pmd_to_swp_entry(*pvmw->pmd);
+
+				if (pfn_swap_entry_to_page(entry) != pvmw->page)
+					return PVM_NOT_MAPPED;
+				return PVM_LEAF_ENTRY;
+			}
+		}
+		return 0;
+	}
+	/* THP pmd was split under us: handle on pte level */
+	spin_unlock(pvmw->ptl);
+	pvmw->ptl = NULL;
+	return PVM_NONLEAF_ENTRY;
+}
+/**
  * page_vma_mapped_walk - check if @pvmw->page is mapped in @pvmw->vma at
  * @pvmw->address
  * @pvmw: pointer to struct page_vma_mapped_walk. page, vma, address and flags
@@ -158,14 +233,9 @@ bool page_vma_mapped_walk(struct page_vma_mapped_walk *pvmw)
 	pgd_t *pgd;
 	p4d_t *p4d;
 	pud_t pude;
-	pmd_t pmde;
 
 	/* The only possible pud mapping has been handled on last iteration */
 	if (!pvmw->pte && !pvmw->pmd && pvmw->pud)
-		return not_found(pvmw);
-
-	/* The only possible pmd mapping has been handled on last iteration */
-	if (pvmw->pmd && !pvmw->pte)
 		return not_found(pvmw);
 
 	if (unlikely(PageHuge(page))) {
@@ -194,10 +264,16 @@ bool page_vma_mapped_walk(struct page_vma_mapped_walk *pvmw)
 	end = PageTransCompound(page) ?
 		vma_address_end(page, pvmw->vma) :
 		pvmw->address + PAGE_SIZE;
+
+	if (pvmw->pmd && !pvmw->pte)
+		goto next_pmd;
+
 	if (pvmw->pte)
 		goto next_pte;
 restart:
 	do {
+		enum check_pmd_result pmd_check_res;
+
 		pgd = pgd_offset(mm, pvmw->address);
 		if (!pgd_present(*pgd)) {
 			step_forward(pvmw, PGDIR_SIZE);
@@ -235,54 +311,47 @@ restart:
 			continue;
 		}
 
-		pvmw->pmd = pmd_offset(pvmw->pud, pvmw->address);
-		/*
-		 * Make sure the pmd value isn't cached in a register by the
-		 * compiler and used as a stale value after we've observed a
-		 * subsequent update.
-		 */
-		pmde = READ_ONCE(*pvmw->pmd);
+		if (!map_pmd(pvmw))
+			goto next_pmd;
+this_pmd:
+		pmd_check_res = check_pmd(pvmw);
+		if (pmd_check_res == PVM_LEAF_ENTRY)
+			return true;
+		else if (pmd_check_res == PVM_NONLEAF_ENTRY)
+			goto pte_level;
 
-		if (pmd_trans_huge(pmde) || is_pmd_migration_entry(pmde)) {
+		/* pmd_check_res == PVM_NOT_MAPPED */
+next_pmd:
+		/* Only PMD-mapped PUD THP has next pmd. */
+		if (!(PageTransHuge(pvmw->page) && compound_order(pvmw->page) == HPAGE_PUD_ORDER))
+			return not_found(pvmw);
+		do {
+			pvmw->address += HPAGE_PMD_SIZE;
+			if (pvmw->address >= end)
+				return not_found(pvmw);
+			/* Did we cross page table boundary? */
+			if ((pvmw->address & (PUD_SIZE - HPAGE_PMD_SIZE)) == 0) {
+				if (pvmw->ptl) {
+					spin_unlock(pvmw->ptl);
+					pvmw->ptl = NULL;
+				}
+				/*
+				 * Reset pmd here, so we will no stay at PMD
+				 * level after restart.
+				 */
+				pvmw->pmd = NULL;
+				goto restart;
+			}
+			pvmw->pmd++;
+			if ((pvmw->flags & PVMW_SYNC) && !pvmw->ptl)
+				pvmw->ptl = pmd_lock(mm, pvmw->pmd);
+		} while (pmd_none(*pvmw->pmd));
+
+		if (!pvmw->ptl)
 			pvmw->ptl = pmd_lock(mm, pvmw->pmd);
-			pmde = *pvmw->pmd;
-			if (likely(pmd_trans_huge(pmde))) {
-				if (pvmw->flags & PVMW_MIGRATION)
-					return not_found(pvmw);
-				if (pmd_page(pmde) != page)
-					return not_found(pvmw);
-				return true;
-			}
-			if (!pmd_present(pmde)) {
-				swp_entry_t entry;
+		goto this_pmd;
 
-				if (!thp_migration_supported() ||
-				    !(pvmw->flags & PVMW_MIGRATION))
-					return not_found(pvmw);
-				entry = pmd_to_swp_entry(pmde);
-				if (!is_migration_entry(entry) ||
-				    pfn_swap_entry_to_page(entry) != page)
-					return not_found(pvmw);
-				return true;
-			}
-			/* THP pmd was split under us: handle on pte level */
-			spin_unlock(pvmw->ptl);
-			pvmw->ptl = NULL;
-		} else if (!pmd_present(pmde)) {
-			/*
-			 * If PVMW_SYNC, take and drop THP pmd lock so that we
-			 * cannot return prematurely, while zap_huge_pmd() has
-			 * cleared *pmd but not decremented compound_mapcount().
-			 */
-			if ((pvmw->flags & PVMW_SYNC) &&
-			    PageTransCompound(page)) {
-				spinlock_t *ptl = pmd_lock(mm, pvmw->pmd);
-
-				spin_unlock(ptl);
-			}
-			step_forward(pvmw, PMD_SIZE);
-			continue;
-		}
+pte_level:
 		if (!map_pte(pvmw))
 			goto next_pte;
 this_pte:
