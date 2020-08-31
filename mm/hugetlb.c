f4245e7a259d6b964622c8b584b575e284639a39
Li Xinhai <lixinhai.lxh@gmail.com>
2020-08-31 02:07:05 +0000

    mm/hugetlb: try preferred node first when alloc gigantic page from cma
    Since commit cf11e85fc08cc6a4 ("mm: hugetlb: optionally allocate gigantic
    hugepages using cma"), the gigantic page would be allocated from node
    which is not the preferred node, although there are pages available from
    that node.  The reason is that the nid parameter has been ignored in
    alloc_gigantic_page().
    
    After this patch, the preferred node is tried first before other allowed
    nodes.
    
    Link: http://lkml.kernel.org/r/20200830140418.605627-1-lixinhai.lxh@gmail.com
    Fixes: cf11e85fc08cc6a4 ("mm: hugetlb: optionally allocate gigantic hugepages using cma")
    Signed-off-by: Li Xinhai <lixinhai.lxh@gmail.com>
    Cc: Roman Gushchin <guro@fb.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1256,8 +1256,15 @@ static struct page *alloc_gigantic_page(struct hstate *h, gfp_t gfp_mask,
 		struct page *page;
 		int node;
 
+		if (hugetlb_cma[nid]) {
+			page = cma_alloc(hugetlb_cma[nid], nr_pages,
+					huge_page_order(h), true);
+			if (page)
+				return page;
+		}
+
 		for_each_node_mask(node, *nodemask) {
-			if (!hugetlb_cma[node])
+			if (node == nid || !hugetlb_cma[node])
 				continue;
 
 			page = cma_alloc(hugetlb_cma[node], nr_pages,
