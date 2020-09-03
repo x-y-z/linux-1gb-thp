6ce286ae395066986a8c3f7fdf2c282af2d1444c
Jason Gunthorpe <jgg@nvidia.com>
2020-09-03 14:08:44 -0300

    mm/pagewalk: use READ_ONCE when reading the PUD entry unlocked
    The pagewalker runs while only holding the mmap_sem for read. The pud can
    be set asynchronously, while also holding the mmap_sem for read
    
    eg from:
    
     handle_mm_fault()
      __handle_mm_fault()
       create_huge_pmd()
        dev_dax_huge_fault()
         __dev_dax_pud_fault()
          vmf_insert_pfn_pud()
           insert_pfn_pud()
            pud_lock()
            set_pud_at()
    
    At least x86 sets the PUD using WRITE_ONCE(), so an unlocked read of
    unstable data should be paired to use READ_ONCE().
    
    For the pagewalker to work locklessly the PUD must work similarly to the
    PMD: once the PUD entry becomes a pointer to a PMD, it must be stable, and
    safe to pass to pmd_offset()
    
    Passing the value from READ_ONCE into the callbacks prevents the callers
    from seeing inconsistencies after they re-read, such as seeing pud_none().
    
    If a callback does obtain the pud_lock then it should trigger ACTION_AGAIN
    if a data race caused the original value to change.
    
    Use the same pattern as gup_pmd_range() and pass in the address of the
    local READ_ONCE stack variable to pmd_offset() to avoid reading it again.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- mm/hmm.c
+++ mm/hmm.c
@@ -402,27 +402,25 @@ static inline unsigned long pud_to_hmm_pfn_flags(struct hmm_range *range,
 	       hmm_pfn_flags_order(PUD_SHIFT - PAGE_SHIFT);
 }
 
-static int hmm_vma_walk_pud(pud_t *pudp, unsigned long start, unsigned long end,
-		struct mm_walk *walk)
+static int hmm_vma_walk_pud(pud_t pud, pud_t *pudp, unsigned long start,
+			    unsigned long end, struct mm_walk *walk)
 {
 	struct hmm_vma_walk *hmm_vma_walk = walk->private;
 	struct hmm_range *range = hmm_vma_walk->range;
 	unsigned long addr = start;
-	pud_t pud;
 	int ret = 0;
 	spinlock_t *ptl = pud_trans_huge_lock(pudp, walk->vma);
 
 	if (!ptl)
 		return 0;
+	if (memcmp(pudp, &pud, sizeof(pud)) != 0) {
+		walk->action = ACTION_AGAIN;
+		spin_unlock(ptl);
+		return 0;
+	}
 
 	/* Normally we don't want to split the huge page */
 	walk->action = ACTION_CONTINUE;
-
-	pud = READ_ONCE(*pudp);
-	if (pud_none(pud)) {
-		spin_unlock(ptl);
-		return hmm_vma_walk_hole(start, end, -1, walk);
-	}
 
 	if (pud_huge(pud) && pud_devmap(pud)) {
 		unsigned long i, npages, pfn;
