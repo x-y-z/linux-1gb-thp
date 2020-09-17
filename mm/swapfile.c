2b3cfe55f8f8383a677b3d8a62c8a52ece03e571
Zi Yan <ziy@nvidia.com>
2020-09-17 11:24:43 -0400

    mm: rmap: add mappped/unmapped page order to anonymous page rmap functions.
    page_add_anon_rmap, do_page_add_anon_rmap, page_add_new_anon_rmap,
    page_remove_rmap are changed to have page order as a parameter. This
    prepares for PMD-mapped PUD THP, since a PUD THP can be mapped in three
    different ways, PTEs, PMDs, and PUDs and the original boolean parameter
    is not enough to record the information.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1919,9 +1919,9 @@ static int unuse_pte(struct vm_area_struct *vma, pmd_t *pmd,
 	set_pte_at(vma->vm_mm, addr, pte,
 		   pte_mkold(mk_pte(page, vma->vm_page_prot)));
 	if (page == swapcache) {
-		page_add_anon_rmap(page, vma, addr, false);
+		page_add_anon_rmap(page, vma, addr, 0);
 	} else { /* ksm created a completely new copy */
-		page_add_new_anon_rmap(page, vma, addr, false);
+		page_add_new_anon_rmap(page, vma, addr, 0);
 		lru_cache_add_inactive_or_unevictable(page, vma);
 	}
 	swap_free(entry);
