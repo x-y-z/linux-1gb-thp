6fa406b4c938d49a4d845168c0cfa322f9a3bc3c
Zi Yan <ziy@nvidia.com>
2020-09-24 12:15:46 -0400

    mm: thp: add anonymous PUD THP page fault support without enabling it.
    This adds PUD THP support for anonymous pages. Applications will be
    able to get PUD pages during page faults when their VMAs are larger than
    PUD page size after the page fault path is enabled.
    
    No shared zero PUD THP is created and shared by all read-only zero PUD
    THPs, different zero read-only PMD THPs. We do not want to reserve so
    much physical memory for this use, assuming the case will be rare.
    
    New PUD THP related events are added too.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- drivers/base/node.c
+++ drivers/base/node.c
@@ -434,6 +434,7 @@ static ssize_t node_read_meminfo(struct device *dev,
 			     "Node %d SUnreclaim:     %8lu kB\n"
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 			     "Node %d AnonHugePages:  %8lu kB\n"
+			     "Node %d AnonHugePUDPages: %8lu kB\n"
 			     "Node %d ShmemHugePages: %8lu kB\n"
 			     "Node %d ShmemPmdMapped: %8lu kB\n"
 			     "Node %d FileHugePages: %8lu kB\n"
@@ -463,6 +464,8 @@ static ssize_t node_read_meminfo(struct device *dev,
 			     ,
 			     nid, K(node_page_state(pgdat, NR_ANON_THPS) *
 				    HPAGE_PMD_NR),
+			     nid, K(node_page_state(pgdat, NR_ANON_THPS_PUD) *
+				    HPAGE_PUD_NR),
 			     nid, K(node_page_state(pgdat, NR_SHMEM_THPS) *
 				    HPAGE_PMD_NR),
 			     nid, K(node_page_state(pgdat, NR_SHMEM_PMDMAPPED) *
--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1209,6 +1209,7 @@ const char * const vmstat_text[] = {
 	"nr_file_hugepages",
 	"nr_file_pmdmapped",
 	"nr_anon_transparent_hugepages",
+	"nr_anon_transparent_pud_hugepages",
 	"nr_vmscan_write",
 	"nr_vmscan_immediate_reclaim",
 	"nr_dirtied",
@@ -1326,6 +1327,9 @@ const char * const vmstat_text[] = {
 	"thp_deferred_split_page",
 	"thp_split_pmd",
 #ifdef CONFIG_HAVE_ARCH_TRANSPARENT_HUGEPAGE_PUD
+	"thp_fault_alloc_pud",
+	"thp_fault_fallback_pud",
+	"thp_fault_fallback_pud_charge",
 	"thp_split_pud",
 #endif
 	"thp_zero_page_alloc",
