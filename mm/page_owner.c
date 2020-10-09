72f2fa5d82ececf6efc0a026094974026a5b6b13
Zi Yan <ziy@nvidia.com>
2020-10-09 18:57:00 -0400

    mm: page_owner: add support for splitting to any order in split page_owner.
    It adds a new_order parameter to set new page order in page owner and
    uses old_order instead of nr to make the parameters look consistent.
    It prepares for upcoming changes to support split huge page to any
    lower order.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -204,19 +204,20 @@ void __set_page_owner_migrate_reason(struct page *page, int reason)
 	page_owner->last_migrate_reason = reason;
 }
 
-void __split_page_owner(struct page *page, unsigned int nr)
+void __split_page_owner(struct page *page, unsigned int old_order,
+			unsigned int new_order)
 {
-	int i;
-	struct page_ext *page_ext = lookup_page_ext(page);
+	int i, old_nr = 1 << old_order, new_nr = 1 << new_order;
+	struct page_ext *page_ext;
 	struct page_owner *page_owner;
 
 	if (unlikely(!page_ext))
 		return;
 
-	for (i = 0; i < nr; i++) {
+	for (i = 0; i < old_nr; i += new_nr) {
+		page_ext = lookup_page_ext(page + i);
 		page_owner = get_page_owner(page_ext);
-		page_owner->order = 0;
-		page_ext = page_ext_next(page_ext);
+		page_owner->order = new_order;
 	}
 }
 
