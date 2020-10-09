6515c0ffbdd6e55c76e7915bff5a3c9dd56331ee
Zi Yan <ziy@nvidia.com>
2020-10-09 18:41:25 -0400

    mm: memcg: make memcg huge page split support any order split.
    It reads thp_nr_pages and splits to provided new_nr. It prepares for
    upcoming changes to support split huge page to any lower order.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -931,7 +931,7 @@ static inline void memcg_memory_event_mm(struct mm_struct *mm,
 }
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-void mem_cgroup_split_huge_fixup(struct page *head);
+void mem_cgroup_split_huge_fixup(struct page *head, unsigned int new_nr);
 void mem_cgroup_split_huge_pud_fixup(struct page *head);
 #endif
 
@@ -1261,7 +1261,8 @@ unsigned long mem_cgroup_soft_limit_reclaim(pg_data_t *pgdat, int order,
 	return 0;
 }
 
-static inline void mem_cgroup_split_huge_fixup(struct page *head)
+static inline void mem_cgroup_split_huge_fixup(struct page *head,
+					       unsigned int new_nr)
 {
 }
 
