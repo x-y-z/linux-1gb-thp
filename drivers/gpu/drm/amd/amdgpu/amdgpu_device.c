53c0adc43bafd231c9455fec0aa0cbfb481fa429
Alex Deucher <alexander.deucher@amd.com>
2020-10-22 18:31:33 -0400

    drm/amdgpu: Enable GPU reset for vangogh
    Enable GPU reset when we encounter a hang.
    
    Acked-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4187,6 +4187,7 @@ bool amdgpu_device_should_recover_gpu(struct amdgpu_device *adev)
 		case CHIP_NAVI14:
 		case CHIP_NAVI12:
 		case CHIP_SIENNA_CICHLID:
+		case CHIP_VANGOGH:
 			break;
 		default:
 			goto disabled;
