255cbaae694d9b635eff22057664e7d2a5c2293e
Roman Gushchin <guro@fb.com>
2020-10-22 15:53:08 -0700

    mm: hugetlb: don't drop hugetlb_lock around cma_release() call
    Replace blocking cma_release() with a non-blocking cma_release_nowait()
    call, so there is no more need to temporarily drop hugetlb_lock.
    
    Signed-off-by: Roman Gushchin <guro@fb.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1273,10 +1273,11 @@ static void free_gigantic_page(struct page *page, unsigned int order)
 {
 	/*
 	 * If the page isn't allocated using the cma allocator,
-	 * cma_release() returns false.
+	 * cma_release_nowait() returns false.
 	 */
 #ifdef CONFIG_CMA
-	if (cma_release(hugetlb_cma[page_to_nid(page)], page, 1 << order))
+	if (cma_release_nowait(hugetlb_cma[page_to_nid(page)], page,
+			       1 << order))
 		return;
 #endif
 
