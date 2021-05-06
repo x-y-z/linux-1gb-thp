a9b3694495a3d73b129c1fcbb675215f00eb1e92
Zi Yan <ziy@nvidia.com>
2021-05-06 11:00:33 -0400

    arch: x86: remove MAX_ORDER exceeding SECTION_SIZE check for 32bit vdso.
    For x86_64, 32bit vdso is compiled for compatibility reason and 32bit
    SECTION_SIZE_BITS value is used during compilation. It causes
    compilation time error when MAX_ORDER is increased in the 64bit
    environment even if it is OK for 64bit SECTION_SIZE_BITS. Remove the
    check during 32bit vdso compilation. The check still exists during the
    compilation of other kernel components.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: x86@kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mm@kvack.org

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -1297,9 +1297,12 @@ static inline bool movable_only_nodes(nodemask_t *nodes)
 #define SECTION_BLOCKFLAGS_BITS \
 	((1UL << (PFN_SECTION_SHIFT - pageblock_order)) * NR_PAGEBLOCK_BITS)
 
+/* NO_MAX_ORDER_CHECK when compiling x64 32bit VDSO for 64bit system */
+#ifndef NO_MAX_ORDER_CHECK
 #if (MAX_ORDER - 1 + PAGE_SHIFT) > SECTION_SIZE_BITS
 #error Allocator MAX_ORDER exceeds SECTION_SIZE
 #endif
+#endif /* NO_MAX_ORDER_CHECK */
 
 static inline unsigned long pfn_to_section_nr(unsigned long pfn)
 {
