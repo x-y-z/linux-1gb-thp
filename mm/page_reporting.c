29b64046cb584d73129f783765883044e8e6ef4b
Zi Yan <ziy@nvidia.com>
2021-07-30 13:28:36 -0400

    mm: make MAX_ORDER a kernel boot time parameter.
    It can be set any value > FORCE_MAX_ZONEORDER, but < 256 (limited by
    vmscan scan_control and per-cpu free page list).
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/page_reporting.c
+++ mm/page_reporting.c
@@ -11,7 +11,7 @@
 #include "page_reporting.h"
 #include "internal.h"
 
-unsigned int page_reporting_order = MAX_ORDER;
+unsigned int page_reporting_order = SECTION_SIZE_BITS - PAGE_SHIFT;
 module_param(page_reporting_order, uint, 0644);
 MODULE_PARM_DESC(page_reporting_order, "Set page reporting order");
 
