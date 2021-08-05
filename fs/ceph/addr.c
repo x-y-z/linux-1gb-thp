willy613/linux

fs: Convert page_mkwrite_check_truncate to folio_mkwrite_check_truncate
If a filesystem uses multi-page folios then its ->page_mkwrite() method
may be called with a tail page.  If it is, the filesystem should treat
the entire folio as being made dirty, so it must iterate over every
extent covered by the folio in case it needs to be COWed.

Change both callers to pass the folio of the page.

Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

fs/ceph/addr.c
@@ -1612,12 +1612,13 @@ static vm_fault_t ceph_page_mkwrite(struct vm_fault *vmf)
 	inode_inc_iversion_raw(inode);
 
 	do {
+		struct folio *folio = page_folio(page);
 		struct ceph_snap_context *snapc;
 
-		lock_page(page);
+		lock_folio(folio);
 
-		if (page_mkwrite_check_truncate(page, inode) < 0) {
-			unlock_page(page);
+		if (folio_mkwrite_check_truncate(folio, inode) < 0) {
+			unlock_folio(folio);
 			ret = VM_FAULT_NOPAGE;
 			break;
 		}
@@ -1625,12 +1626,12 @@ static vm_fault_t ceph_page_mkwrite(struct vm_fault *vmf)
 		snapc = ceph_find_incompatible(page);
 		if (!snapc) {
 			/* success.  we'll keep the page locked. */
-			set_page_dirty(page);
+			set_folio_dirty(folio);
 			ret = VM_FAULT_LOCKED;
 			break;
 		}
 
-		unlock_page(page);
+		unlock_folio(folio);
 
 		if (IS_ERR(snapc)) {
 			ret = VM_FAULT_SIGBUS;

x-y-z/linux-1gb-thp

fs/ceph/addr.c
@@ -1489,12 +1489,13 @@ static vm_fault_t ceph_page_mkwrite(struct vm_fault *vmf)
 	inode_inc_iversion_raw(inode);
 
 	do {
+		struct folio *folio = page_folio(page);
 		struct ceph_snap_context *snapc;
 
-		lock_page(page);
+		folio_lock(folio);
 
-		if (page_mkwrite_check_truncate(page, inode) < 0) {
-			unlock_page(page);
+		if (folio_mkwrite_check_truncate(folio, inode) < 0) {
+			folio_unlock(folio);
 			ret = VM_FAULT_NOPAGE;
 			break;
 		}
@@ -1502,12 +1503,12 @@ static vm_fault_t ceph_page_mkwrite(struct vm_fault *vmf)
 		snapc = ceph_find_incompatible(page);
 		if (!snapc) {
 			/* success.  we'll keep the page locked. */
-			set_page_dirty(page);
+			folio_mark_dirty(folio);
 			ret = VM_FAULT_LOCKED;
 			break;
 		}
 
-		unlock_page(page);
+		folio_unlock(folio);
 
 		if (IS_ERR(snapc)) {
 			ret = VM_FAULT_SIGBUS;