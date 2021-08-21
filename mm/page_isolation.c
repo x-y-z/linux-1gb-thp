a2c9cdf6c80df2681be3daa442a7bfe61914ecdd
Zi Yan <ziy@nvidia.com>
2021-08-20 22:21:22 -0400

    mm: rectify MAX_ORDER semantics to be the largest page order from buddy allocator
    MAX_ORDER used to denote the largest page order + 1, but that was
    confusing and caused several off-by-1 errors in the code. Fix it by
    setting MAX_ORDER to the largest page order from buddy allocator like
    what its name says.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/page_isolation.c
+++ mm/page_isolation.c
@@ -226,7 +226,7 @@ static void unset_migratetype_isolate(struct page *page, int migratetype)
 	 */
 	if (PageBuddy(page)) {
 		order = buddy_order(page);
-		if (order >= pageblock_order && order < MAX_ORDER - 1) {
+		if (order >= pageblock_order && order <= MAX_ORDER) {
 			buddy = find_buddy_page_pfn(page, page_to_pfn(page),
 						    order, NULL);
 			if (buddy && !is_migrate_isolate_page(buddy)) {
@@ -286,11 +286,11 @@ __first_valid_page(unsigned long pfn, unsigned long nr_pages)
  * @gfp_flags:			GFP flags used for migrating pages
  * @isolate_before:	isolate the pageblock before the boundary_pfn
  *
- * Free and in-use pages can be as big as MAX_ORDER-1 and contain more than one
+ * Free and in-use pages can be as big as MAX_ORDER and contain more than one
  * pageblock. When not all pageblocks within a page are isolated at the same
  * time, free page accounting can go wrong. For example, in the case of
- * MAX_ORDER-1 = pageblock_order + 1, a MAX_ORDER-1 page has two pagelbocks.
- * [         MAX_ORDER-1         ]
+ * MAX_ORDER = pageblock_order + 1, a MAX_ORDER page has two pagelbocks.
+ * [         MAX_ORDER           ]
  * [  pageblock0  |  pageblock1  ]
  * When either pageblock is isolated, if it is a free page, the page is not
  * split into separate migratetype lists, which is supposed to; if it is an
@@ -403,13 +403,13 @@ static int isolate_single_pageblock(unsigned long boundary_pfn, gfp_t gfp_flags,
 			 * split the free page to the right migratetype list.
 			 *
 			 * head_pfn is not used here as a hugetlb page order
-			 * can be bigger than MAX_ORDER-1, but after it is
+			 * can be bigger than MAX_ORDER, but after it is
 			 * freed, the free page order is not. Use pfn within
 			 * the range to find the head of the free page and
 			 * reset order to 0 if a hugetlb page with
-			 * >MAX_ORDER-1 order is encountered.
+			 * >MAX_ORDER order is encountered.
 			 */
-			if (order > MAX_ORDER-1)
+			if (order > MAX_ORDER)
 				order = 0;
 			while (!PageBuddy(pfn_to_page(pfn))) {
 				order++;
@@ -586,7 +586,7 @@ int test_pages_isolated(unsigned long start_pfn, unsigned long end_pfn,
 	int ret;
 
 	/*
-	 * Note: pageblock_nr_pages != MAX_ORDER. Then, chunks of free pages
+	 * Note: pageblock_order != MAX_ORDER. Then, chunks of free pages
 	 * are not aligned to pageblock_nr_pages.
 	 * Then we just check migratetype first.
 	 */
