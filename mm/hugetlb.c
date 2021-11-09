5a60ef216569c1783066821253d60d69dd4411b6
Zi Yan <ziy@nvidia.com>
2021-11-09 14:46:29 -0500

    mm: replace MAX_ORDER when it is used to indicate max physical contiguity.
    MAX_ORDER is limited at a memory section size, thus widely used as
    a variable to indicate maximum physically contiguous page size. But this
    limitation is no longer necessary as kernel only supports sparse memory
    model. Add a new variable MAX_PHYS_CONTIG_ORDER to replace such uses of
    MAX_ORDER.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1887,7 +1887,7 @@ pgoff_t hugetlb_basepage_index(struct page *page)
 	pgoff_t index = page_index(page_head);
 	unsigned long compound_idx;
 
-	if (compound_order(page_head) > MAX_ORDER)
+	if (compound_order(page_head) > MAX_PHYS_CONTIG_ORDER)
 		compound_idx = page_to_pfn(page) - page_to_pfn(page_head);
 	else
 		compound_idx = page - page_head;
