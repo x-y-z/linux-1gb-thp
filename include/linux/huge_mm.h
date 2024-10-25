ddd2ff0f91828f60172288124f39801e9d06718f
Zi Yan <ziy@nvidia.com>
2024-10-25 16:07:51 -0400

    mm/truncate: use folio_split() for truncate operation.
    Instead of splitting the large folio uniformly during truncation, use
    buddy allocator like split at the start of truncation range to minimize
    the number of resulting folios.
    
    For example, to truncate a order-4 folio
    [0, 1, 2, 3, 4, 5, ..., 15]
    between [3, 10] (inclusive), folio_split() splits the folio to
    [0,1], [2], [3], [4..7], [8..15] and [3], [4..7] can be dropped and
    [8..15] is kept with zeros in [8..10], then another folio_split() is
    done at 10, so [8..10] can be dropped.
    
    One possible optimization is to make folio_split() to split a folio
    based on a given range, like [3..10] above. But that complicates
    folio_split(), so it will be investigated when necessary.
    
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -341,6 +341,17 @@ int split_huge_page_to_list_to_order(struct page *page, struct list_head *list,
 		unsigned int new_order);
 int min_order_for_split(struct folio *folio);
 int split_folio_to_list(struct folio *folio, struct list_head *list);
+int folio_split(struct folio *folio, unsigned int new_order, struct page *page,
+		struct list_head *list);
+static inline int split_folio_at(struct folio *folio, struct page *page,
+		struct list_head *list)
+{
+	int ret = min_order_for_split(folio);
+
+	if (ret < 0)
+		return ret;
+	return folio_split(folio, ret, page, list);
+}
 static inline int split_huge_page(struct page *page)
 {
 	struct folio *folio = page_folio(page);
@@ -529,6 +540,12 @@ static inline int split_huge_page(struct page *page)
 }
 
 static inline int split_folio_to_list(struct folio *folio, struct list_head *list)
+{
+	return 0;
+}
+
+static inline int split_folio_at(struct folio *folio, struct page *page,
+		struct list_head *list)
 {
 	return 0;
 }
